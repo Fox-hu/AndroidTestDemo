f0959f0bf13e19ad1e16a436d5730521e0dfb9ea
Fox.hu <Hujia0523@gmail.com>
2019-01-24 23:14:58 +0800

    add demo

--- materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/collapsingToolbarLayout/JobDemo1Activity.java
+++ materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/collapsingToolbarLayout/JobDemo1Activity.java
@@ -1,37 +1,30 @@
 package com.example.materialdesigntestdemo.collapsingToolbarLayout;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.materialdesigntestdemo.R;
-
-import java.util.Arrays;
-import java.util.List;
 
 
 public class JobDemo1Activity extends AppCompatActivity {
     private static final String TAG = JobDemo1Activity.class.getSimpleName();
     private NestedScrollView Low;
     private RecyclerView rv;
-    private String[] data = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i"};
+//    private String[] data = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_demo1);
-        Low = findViewById(R.id.low_scrollview);
-        Low.setOnScrollChangeListener(lowListener);
+//        Low = findViewById(R.id.low_scrollview);
+//        Low.setOnScrollChangeListener(lowListener);
 
-        rv = findViewById(R.id.rv_content);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.setAdapter(new TestAdapter(R.layout.rv_item, Arrays.asList(data)));
+//        rv = findViewById(R.id.rv_content);
+//        rv.setLayoutManager(new LinearLayoutManager(this));
+//        rv.setAdapter(new TestAdapter(R.layout.rv_item, Arrays.asList(data)));
     }
 
     private NestedScrollView.OnScrollChangeListener lowListener = new NestedScrollView.OnScrollChangeListener() {
@@ -45,23 +38,23 @@ public class JobDemo1Activity extends AppCompatActivity {
     };
 
 
-    private class TestAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
-
-        public TestAdapter(int layoutResId, @Nullable List<String> data) {
-            super(layoutResId, data);
-        }
-
-        public TestAdapter(@Nullable List<String> data) {
-            super(data);
-        }
-
-        public TestAdapter(int layoutResId) {
-            super(layoutResId);
-        }
-
-        @Override
-        protected void convert(BaseViewHolder helper, String item) {
-            helper.setText(R.id.hot_word_textview, item);
-        }
-    }
+//    private class TestAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+//
+//        public TestAdapter(int layoutResId, @Nullable List<String> data) {
+//            super(layoutResId, data);
+//        }
+//
+//        public TestAdapter(@Nullable List<String> data) {
+//            super(data);
+//        }
+//
+//        public TestAdapter(int layoutResId) {
+//            super(layoutResId);
+//        }
+//
+//        @Override
+//        protected void convert(BaseViewHolder helper, String item) {
+//            helper.setText(R.id.hot_word_textview, item);
+//        }
+//    }
 }
