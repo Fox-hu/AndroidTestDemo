902c9dc0eed9968962fe456e3ae09d488fb39f80
Fox.hu <Hujia0523@gmail.com>
2019-01-29 13:20:26 +0800

    bug修正

--- materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/collapsingToolbarLayout/JobDemo/JobDemo3Activity.java
+++ materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/collapsingToolbarLayout/JobDemo/JobDemo3Activity.java
@@ -2,7 +2,6 @@ package com.example.materialdesigntestdemo.collapsingToolbarLayout.JobDemo;
 
 import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -22,9 +21,9 @@ public class JobDemo3Activity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private ImageView head;
     private boolean isShow = true;
-    private volatile boolean isScrollStop = true;
     private int headHeight = Utils.dip2px(InitApp.sContext, 1000);
     private boolean isAnimating = false;
+    private boolean isFingerUp = false;
     private int oldscrollY;
 
     @Override
@@ -36,19 +35,6 @@ public class JobDemo3Activity extends AppCompatActivity {
 
         nestedScrollView = findViewById(R.id.nested_scrollview);
         nestedScrollView.setOnScrollChangeListener(scrollChangeListener);
-        nestedScrollView.setStopListener(new MyNestedScrollView2.StopListener() {
-            @Override
-            public void onScrollStop() {
-                //stop时，OnScrollChangeListener仍在运行 并没有获取点击
-                Log.e(TAG, "MyNestedScrollView2 onstop ");
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        isScrollStop = true;
-                    }
-                }, 1000);
-            }
-        });
 
         rv = findViewById(R.id.rv_content);
         Utils.setRv(rv, this);
@@ -78,25 +64,25 @@ public class JobDemo3Activity extends AppCompatActivity {
             //                    }
             //                }
             //            }
-            Log.e(TAG, "NestedScrollView onScrollChange ," + " getMaxScrollAmount = " +
-                       v.getMaxScrollAmount() + " scrollX = " + scrollX + " scrollY = " + scrollY +
-                       " oldScrollX = " + oldScrollX + " oldScrollY = " + oldScrollY);
+            Log.e(TAG,
+                    "NestedScrollView onScrollChange ,  scrollY = " + scrollY + " oldScrollY = " +
+                    oldScrollY + " preScrolly = " + oldscrollY);
 
+            //注意 y值可能会超过head的大小
             if (scrollY - oldscrollY > 0 && !Utils.isVisible(rv) && !isAnimating) {
-                isScrollStop = false;
                 Log.e(TAG, "ScrollEvent bringToMid");
                 bringToMid();
+                oldscrollY = scrollY;
                 return;
             }
 
-            if (scrollY - oldscrollY > 0 && isShow && !isAnimating && isScrollStop) {
-                isScrollStop = false;
-                isShow = false;
+            if (2000 + 200 < scrollY && scrollY < 3500 && scrollY - oldscrollY > 0 &&
+                !isAnimating) {
                 Log.e(TAG, "ScrollEvent onscrollup");
                 hideHead();
-            } else if (scrollY - oldscrollY < 0 && !isShow && !isAnimating && isScrollStop) {
-                isScrollStop = false;
-                isShow = true;
+            }
+
+            if (oldscrollY <= 3500 && scrollY - oldscrollY < 0  && !isAnimating) {
                 Log.e(TAG, "ScrollEvent onscrolldown");
                 showHead();
             }
@@ -136,19 +122,21 @@ public class JobDemo3Activity extends AppCompatActivity {
     }
 
     public void bringToMid() {
-        isScrollStop = false;
         if (isAnimating) {
             return;
         }
+        final int scrollY = nestedScrollView.getScrollY();
+        final int i = 2000 - scrollY;
         ValueAnimator valueAnimator = new ValueAnimator();
-        valueAnimator.setIntValues(0, 2000);
-        valueAnimator.setDuration(1000);
+        valueAnimator.setIntValues(0, i);
+        valueAnimator.setDuration(500);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 int value = (int) animation.getAnimatedValue();
-                isAnimating = value != 2000;
-                nestedScrollView.scrollTo(0, value);
+                Log.e(TAG, "bringToMid , animation value = " + value + " i = " + i);
+                nestedScrollView.scrollTo(0, scrollY + value);
+                isAnimating = value != i;
             }
         });
         valueAnimator.start();
@@ -184,15 +172,18 @@ public class JobDemo3Activity extends AppCompatActivity {
         if (isAnimating) {
             return;
         }
+        final int scrollY = nestedScrollView.getScrollY();
+        final int i = 3500 - scrollY;
         ValueAnimator valueAnimator = new ValueAnimator();
-        valueAnimator.setIntValues(0, 1500);
-        valueAnimator.setDuration(1000);
+        valueAnimator.setIntValues(0, i);
+        valueAnimator.setDuration(500);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 int value = (int) animation.getAnimatedValue();
-                isAnimating = value != 1500;
-                nestedScrollView.scrollTo(0, 2000 + value);
+                Log.e(TAG, "head hide , animation value = " + value + " i = " + i);
+                nestedScrollView.scrollTo(0, scrollY + value);
+                isAnimating = value != i;
             }
         });
         valueAnimator.start();
