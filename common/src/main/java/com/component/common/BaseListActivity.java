e053629a9bf082403f32c5b6ab806d63de9edf37
Fox.hu <Hujia0523@gmail.com>
2019-01-31 13:22:05 +0800

    demo的主页面抽出

---
+++ common/src/main/java/com/component/common/BaseListActivity.java
@@ -0,0 +1,47 @@
+package com.component.common;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public abstract class BaseListActivity extends AppCompatActivity {
+
+    private ListView lv;
+    protected Map<String, Class<?>> activityMap = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.lv);
+
+        initItem();
+
+        ArrayAdapter activityAdatper = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, new ArrayList<>(activityMap.keySet()));
+        lv.setAdapter(activityAdatper);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (view instanceof TextView) {
+                    String s = ((TextView) view).getText().toString();
+                    startActivity(new Intent(BaseListActivity.this, activityMap.get(s)));
+                }
+            }
+        });
+
+    }
+
+    protected abstract void initItem();
+}
--- materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/MainActivity.java
+++ materialdesigntestdemo/src/main/java/com/example/materialdesigntestdemo/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.materialdesigntestdemo;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
+import com.component.common.BaseListActivity;
 import com.example.materialdesigntestdemo.collapsingToolbarLayout.CollapsingNormalActivity;
 import com.example.materialdesigntestdemo.collapsingToolbarLayout.JobDemo.JobDemo1Activity;
 import com.example.materialdesigntestdemo.collapsingToolbarLayout.JobDemo.JobDemo3Activity;
@@ -19,39 +11,11 @@ import com.example.materialdesigntestdemo.collapsingToolbarLayout.SearchActivity
 import com.example.materialdesigntestdemo.collapsingToolbarLayout.SerachAppBarActivity;
 import com.example.materialdesigntestdemo.collapsingToolbarLayout.WebViewCollapsingTestActivity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private ListView lv;
-    private Map<String, Class<?>> activityMap = new HashMap<>();
+public class MainActivity extends BaseListActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        lv = findViewById(R.id.lv);
-
-        initItem();
-
-        ArrayAdapter activityAdatper = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, new ArrayList<>(activityMap.keySet()));
-        lv.setAdapter(activityAdatper);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (view instanceof TextView) {
-                    String s = ((TextView) view).getText().toString();
-                    startActivity(new Intent(MainActivity.this, activityMap.get(s)));
-                }
-            }
-        });
-    }
-
-    private void initItem() {
+    protected void initItem() {
         activityMap.put("CollapsingNormalActivity", CollapsingNormalActivity.class);
         activityMap.put("ScrollingActivity", ScrollingActivity.class);
         activityMap.put("SearchActivity", SearchActivity.class);
