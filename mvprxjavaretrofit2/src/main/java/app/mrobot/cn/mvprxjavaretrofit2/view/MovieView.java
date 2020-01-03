725cdc221f31f6e63fd7114b49327fce70e6a330
Fox.hu <Hujia0523@gmail.com>
2020-01-03 20:34:41 +0800

    remove mvp mvvm demo
    remove mvp mvvm demo

--- mvprxjavaretrofit2/src/main/java/app/mrobot/cn/mvprxjavaretrofit2/view/MovieView.java
+++
@@ -1,9 +0,0 @@
-package app.mrobot.cn.mvprxjavaretrofit2.view;
-
-/**
- * Created by admin on 2018/1/8.
- */
-
-public interface MovieView extends BaseView{
-
-}
--- mvvmrxjavaretrofit2/src/main/java/app/mrobot/cn/demos/view/CompletedListener.java
+++
@@ -1,8 +0,0 @@
-package app.mrobot.cn.demos.view;
-
-/**
- * Created by admin on 2017/2/12.
- */
-public interface CompletedListener {
-    void onCompleted();
-}
